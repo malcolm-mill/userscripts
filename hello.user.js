08095ed8a36efa7138f1028de52007e8a4ff9753
malcolm mill <maldermill@googlemail.com>
2020-06-02 22:09:29 +0100

    initial commit

---
+++ hello.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Hello World
+// @namespace    http://github.com/malcolm-mill
+// @version      0.1
+// @description  try to take over the world!
+// @author       malcolm-mill
+// @match        http://*/*
+// @match        https://*/*
+// @require http://code.jquery.com/jquery-latest.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+   console.log("hello, world!");
+   alert('hello world');
+    //$('body').append('<input type="button" value="Open" id="CP">');
+    //$("#CP").css("position", "fixed").css("top", 0).css("left", 0);
+})();
