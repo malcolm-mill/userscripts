5b8b9db108848fad7679928a794041e2632f28fb
malcolm mill <maldermill@googlemail.com>
2020-06-02 23:02:06 +0100

    floating button script added

---
+++ floating-button.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         floating button
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @require      http://code.jquery.com/jquery-latest.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var input=document.createElement("input");
+    input.type="button";
+    input.value="GreaseMonkey Button";
+    input.onclick = showAlert;
+    input.setAttribute("style", "font-size:18px;position:fixed;display:block;padding:8px;top:100px;left:8px;");
+    document.body.appendChild(input);
+
+    function showAlert()
+    {
+        alert("Hello World");
+    }
+})();
